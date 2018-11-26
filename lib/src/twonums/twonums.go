2bd9227c10423581db407878daa0379e9ae149ff
power <powerting@163.com>
2018-11-26 20:29:22 +0800

    update

---
+++ lib/src/twonums/twonums.go
@@ -0,0 +1,15 @@
+/*
+this game is for XiXi and YuanYuan
+game ruler:
+1.XiXi apply two num for YuanYuan to calculate
+2.if YuanYuan's return is right then YuanYuan apply two num for XiXi calculate
+3.repet 1 and 2
+*/
+package twonums
+
+import "fmt"
+
+func Twonums() bool {
+	fmt.Println("Ok XiXi and YuanYuan Now Play twonums Game")
+	return true
+}
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
+/*
+this application is for XiXi and YuanYuan play
+1.first XiXi or YuanYuan select a game, Then they played
+2.find who win
+3.repet 1 and 2
+*/
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"twonums"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Hello XiXi and YuanYuan")
+	twonums.Twonums()
+
+}
+
+//! Test Func
+func TestMain() {
+	fmt.Println("ok you passed the Test!")
 }
